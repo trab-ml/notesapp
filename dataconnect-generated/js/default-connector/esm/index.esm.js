b6264c23ff4499b016437cba1660e4d02767da43
Traore Bernard <bernard_traore@ens.univ-artois.fr>
2025-03-29 17:48:29 +0100

    Add firebase to the project

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'notesapp',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'notesapp',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
